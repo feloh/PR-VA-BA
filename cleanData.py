ffccad60d017512a3d4061f0a9f41c8e52b66826
felixlohmann <feloh@gmx.de>
2022-11-20 20:32:57 +0100

    Cleaning Data fix Datatype cleareance Output to CSV Sketch for Deep Learning in main

--- cleanData.py
+++ cleanData.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from pyspin.spin import make_spin, Default
+import numpy as np
 from tqdm import tqdm
+import gc
 
 
 # Import Function Excel --> Data
 @make_spin(Default, "Reading Excel Sheet...")
 def import_data(path, name):
     print('Path: ', path)
-    data_frame = pd.read_excel(path, sheet_name=name)
+    data_frame = pd.read_excel(path, sheet_name=name, dtype={'BallPosession': np.int64, 'MatchTime': np.int64,
+                                                             'BallSpeed': np.int64, 'ReplaysTotal': np.int64,
+                                                             'GameInterruptionsDiverse': np.int64})
     return data_frame
 
 
@@ -39,53 +43,47 @@ def smooth_data(dataframe, column, operation, r, timestamp_limit):
     # Paired Flags contain the first and the last Flag of a missing data part
     paired_flags = list(zip(flags[0::2], flags[1::2]))
     print(len(paired_flags), ' paired Flags')
+    del flags
+    gc.collect()
+    if len(paired_flags) != 0:
+        # Smooth out gaps in dependency of the Data and the Data Type
+        print('Correcting the values in column ', column)
+        for pair in tqdm(paired_flags):
+            # Steigung
+            val_dif = pair[1][1] - pair[0][1]
+            # Datenlücke
+            time_dif = pair[1][0] - pair[0][0]
+            print(time_dif)
 
-    # Smooth out gaps in dependency of the Data and the Data Type
-    print('Correcting the values in column ', column)
-    for pair in tqdm(paired_flags):
-        # Steigung
-        val_dif = pair[1][1] - pair[0][1]
-        # Datenlücke
-        time_dif = pair[1][0] - pair[0][0]
-
-        if time_dif <= timestamp_limit:
-            if operation == 'splitGap':
-                split_one = time_dif//2
-                split_two = time_dif - split_one
-                for i in range(split_one):
-                    dataframe.at[pair[0][0] + i + 1, column] = pair[0][1]
-                for i in reversed(range(split_two)):
-                    dataframe.at[pair[1][0] - i - 1, column] = pair[1][1]
-            elif operation == 'gradient':
-                m = val_dif / time_dif
-                for i in range(time_dif):
-                    if r:
-                        dataframe.at[pair[0][0] + i + 1, column] = round(pair[0][1] + m * (i + 1))
-                    else:
-                        dataframe.at[pair[0][0] + i + 1, column] = pair[0][1] + m * (i + 1)
+            if time_dif <= timestamp_limit:
+                if operation == 'splitGap':
+                    split_one = time_dif//2
+                    split_two = time_dif - split_one
+                    for i in range(split_one):
+                        dataframe.at[pair[0][0] + i + 1, column] = pair[0][1]
+                    for i in reversed(range(split_two)):
+                        dataframe.at[pair[1][0] - i - 1, column] = pair[1][1]
+                elif operation == 'gradient':
+                    m = val_dif / time_dif
+                    for i in range(time_dif):
+                        if r:
+                            dataframe.at[pair[0][0] + i + 1, column] = round(pair[0][1] + m * (i + 1))
+                        else:
+                            dataframe.at[pair[0][0] + i + 1, column] = pair[0][1] + m * (i + 1)
+    del paired_flags
+    gc.collect()
 
 
 data_path = 'data/DataSet_270722.xlsx'
 data_sheet_name = 'Data'
 Data_to_Smooth = [
-    dict(name='HR', operation='gradient', timeStampLimit=300, r=True),
-    dict(name='EMGDelta', operation='gradient', timeStampLimit=300, r=False),
-    dict(name='PitchZones', operation='splitGap', timeStampLimit=300, r=True),
-    dict(name='ObjectsOnScreen', operation='splitGap', timeStampLimit=300, r=True)
+    dict(name='HR', operation='gradient', timeStampLimit=120, r=True),
+    dict(name='EMGDelta', operation='gradient', timeStampLimit=60, r=False),
+    dict(name='PitchZones', operation='splitGap', timeStampLimit=30, r=True),
+    dict(name='ObjectsOnScreen', operation='splitGap', timeStampLimit=30, r=True),
+    dict(name='BallPosession', operation='splitGap', timeStampLimit=30, r=True),
+    dict(name='OddsDiffSquare', operation='gradient', timeStampLimit=300, r=True)
 ]
-export_path = 'data/Output.xlsx'
-
-# -- Import the Dataset from Excel --
-df = import_data(data_path, data_sheet_name)
-print('The Data has been successfully imported.')
-print(df.info())
-
-# -- Clean the Dataset --
-# Remove the BallSpeed Column
-df = df.drop(columns='BallSpeed')
-print('-- Dropped BallSpeed --')
-
-# Remove not combined Columns
 not_combined_columns = [
     'BallPosessionAway',
     'BallPosessionHome',
@@ -117,27 +115,37 @@ not_combined_columns = [
     'Zone5',
     'Zone6',
 ]
+export_path = 'data/Output.xlsx'
+export_path_csv = 'data/Output.csv'
+
+# -- Import the Dataset from Excel --
+df = import_data(data_path, data_sheet_name)
+print('The Data has been successfully imported.')
+print(df.info())
+
+# -- Clean the Dataset --
+# Remove the BallSpeed Column
+df = df.drop(columns='BallSpeed')
+print('-- Dropped BallSpeed --')
+
+# Remove not combined Columns
 df = df.drop(columns=not_combined_columns)
 print('-- Dropped not combined Columns --')
+del not_combined_columns
+gc.collect()
 
 # Smooth Missing Data
 for data in Data_to_Smooth:
-    smooth_data(dataframe=df, column=data['name'], operation=data['operation'], r=data['r'],
-                timestamp_limit=data['timeStampLimit'])
+    smooth_data(dataframe=df, column=data['name'], operation=data['operation'],
+                timestamp_limit=data['timeStampLimit'], r=data['r'])
 
-# Drop Participants with missing Data
-participant_id = []
+
+# Drop Rows with missing Data
 print('Searching for missing Data...')
 for data in tqdm(Data_to_Smooth):
-    for row in df.index[df[data['name']] == -99]:
-        print(data['name'])
-        participant_id.append(df.at[row, 'ParticipantNumber'])
-set_pi = set(participant_id)
-unique_participant_id = list(set_pi)
-for upi in unique_participant_id:
-    print('Dropping Participant ', upi, 'for lack of Data.')
-    df = df.drop(df.index[df['ParticipantNumber'] == upi])
+    df = df.drop(index=df.index[df[data['name']] == -99])
 
 # -- Export and Save the Dataframe --
 print('Export the Data to', export_path)
 df.to_excel(export_path)
+df.to_csv(export_path_csv)
